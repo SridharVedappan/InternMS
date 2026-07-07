6854b295db1084af5d3049a9f1ff7b3be8ed90ce
SridharVedappan <sridharvedappan@thestackly.com>
2026-07-07 18:34:23 +0530

    updated

---
+++ landing-page/src/components/LoginPages/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../LoginPages/ForgotPassword.css";
+
+export default function forgotPassword() {
+  return (
+    <div>
+      <div className="left-container-for-forgot-password"></div>
+      <div className="right-container-for-forgot-password"></div>
+    </div>
+  );
+}
